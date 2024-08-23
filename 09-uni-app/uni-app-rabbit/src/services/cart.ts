c8206301b5ff84bcaec9dabb7fc5183ed829c240
czm <zhengmao.chen@upai.com>
2024-08-23 18:00:39 +0800

    add

--- 09-uni-app/uni-app-rabbit/src/services/cart.ts
+++ 09-uni-app/uni-app-rabbit/src/services/cart.ts
@@ -24,6 +24,23 @@ export const deleteMemberCartAPI = (data: { ids: string[] }) => {
 }
 
 /**
+ * 修改购物车单品
+ * @param skuId SKUID
+ * @param data selected 选中状态 count 商品数量
+ */
+export const putMemberCartBySkuIdAPI = (
+  skuId: string,
+  data: { selected?: boolean; count?: number },
+) => {
+  return http({
+    method: 'PUT',
+    url: `/member/cart/${skuId}`,
+    data,
+  })
+}
+
+
+/**
  * 加入购物车
  */
 export const postMemberCartAPI = (data: {skuId: string, count: number}) => {
@@ -34,3 +51,15 @@ export const postMemberCartAPI = (data: {skuId: string, count: number}) => {
   })
 }
 
+
+/**
+ * 购物车全选/取消全选
+ * @param data selected 是否选中
+ */
+export const putMemberCartSelectedAPI = (data: { selected: boolean }) => {
+  return http({
+    method: 'PUT',
+    url: '/member/cart/selected',
+    data,
+  })
+}
